d4478eb1690df91c81748520b4ea73475d65aff8
geno223 <149203931+geno223@users.noreply.github.com>
2023-11-20 20:25:45 -0500

    Update answers.js

--- homework/answers.js
+++ homework/answers.js
@@ -100,3 +100,86 @@ for (let i = 1; i <= 20; i++) {
   }
 }
 
+
+/// Where is Waldo answered
+
+
+const whereIsWaldo = 
+[["Timmy", "Frank"],
+
+"Eggbert",
+                    
+["Lucinda", "Jacc", "Neff", "Snoop"],
+                  
+["Petunia", 
+
+  ["Baked Goods", "Waldo"]]];
+
+console.log(whereIsWaldo[1])
+
+whereIsWaldo.splice(1, 1)
+console.log(whereIsWaldo)
+
+//changing neff to no one
+
+whereIsWaldo[1][2] = "No One"
+
+console.log(whereIsWaldo)
+
+//Access and console.log "Waldo"
+whereIsWaldo[2][1][1]
+
+console.log(whereIsWaldo[2][1][1])
+
+
+//Methods, Revisited
+
+
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+// logging titanic index
+ console.log(favMovies[8])
+//  use the .sort method Thought question: what did this do to the array? Did it permanently alter it?
+ favMovies.sort()
+
+ console.log(favMovies)
+
+ //use the methhod pop
+
+ favMovies.pop()
+
+ console.log(favMovies)
+//push "Guardians of the Galaxy"
+
+favMovies.push("Guardians of The Galaxy")
+
+console.log(favMovies)
+
+//Reverse the array
+
+favMovies.reverse()
+
+console.log(favMovies)
+
+//Use the shift method
+
+console.log(favMovies.shift())
+
+console.log(favMovies)
+
+//unshift - what does it return?
+//returns lentgh of array
+favMovies.unshift()
+
+console.log(favMovies.unshift())
+
+//splice "Django Unchained" and add "Avatar" 
+//(try finding the index of "Django Unchained", instead of counting it yourself) 
+//Thought question: did this permanently alter our array?
+
+favMovies.indexOf("Django Unchained")
+
+console.log(favMovies.indexOf("Django Unchained"))
+
+favMovies.splice(14,1,"Avatar")
+
+console.log(favMovies)
